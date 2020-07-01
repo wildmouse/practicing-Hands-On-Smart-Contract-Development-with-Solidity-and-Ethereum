e4451266abb9ac345c49ddac5e173d675ebe4003
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-02 08:11:25 +0900

    Add greet function

--- greeter/contracts/Greeter.sol
+++ greeter/contracts/Greeter.sol
@@ -1,5 +1,7 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
 contract Greeter {
-
+    function greet() external pure returns(string memory) {
+        return "Hello, World!";
+    }
 }
--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -1,8 +1,18 @@
 const GreeterContract = artifacts.require("Greeter")
 
-contract("Greeter", () => { 
-    it("has been deployed successfully", async () => { 
+contract("Greeter", () => {
+    it("has been deployed successfully", async () => {
         const greeter = await GreeterContract.deployed();
         assert(greeter, "contract was not deployed");
+    })
+})
+
+describe("greet()", () => {
+    it("returns 'Hello, World!'", async () => {
+        const greeter = await GreeterContract.deployed();
+        const expected = "Hello, World!";
+        const actual = await greeter.greet();
+
+        assert.equal(actual, expected, "greeted with 'Hello, World!'");
     })
 })
