6db6ebf224674785ef3d7bc82b5dee9a8f318b9c
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-02 08:39:35 +0900

    Add setting greeting function

--- greeter/contracts/Greeter.sol
+++ greeter/contracts/Greeter.sol
@@ -1,7 +1,13 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
 contract Greeter {
-    function greet() external pure returns(string memory) {
-        return "Hello, World!";
+    string private _greeting = "Hello, World!";
+
+    function greet() external view returns(string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
     }
 }
--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -5,14 +5,28 @@ contract("Greeter", () => {
         const greeter = await GreeterContract.deployed();
         assert(greeter, "contract was not deployed");
     })
+
+    describe("greet()", () => {
+        it("returns 'Hello, World!'", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello, World!";
+            const actual = await greeter.greet();
+
+            assert.equal(actual, expected, "greeted with 'Hello, World!'");
+        })
+    })
 })
 
-describe("greet()", () => {
-    it("returns 'Hello, World!'", async () => {
-        const greeter = await GreeterContract.deployed();
-        const expected = "Hello, World!";
-        const actual = await greeter.greet();
+contract("Greeter: update greeting", () => {
+    describe("setGreeting(string)", () => {
+        it("sets greeting to passed in string", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hi, there!";
 
-        assert.equal(actual, expected, "greeted with 'Hello, World!'");
+            await greeter.setGreeting(expected);
+            const actual = await greeter.greet();
+
+            assert.equal(actual, expected, "greeting was not updated");
+        })
     })
 })
