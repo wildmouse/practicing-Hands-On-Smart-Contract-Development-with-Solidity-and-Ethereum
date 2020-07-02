04aacb5b477c7c03aef27cde7c49f16763dac759
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-03 08:03:52 +0900

    Add only owner modifier

--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -34,16 +34,34 @@ contract("Greeter", (accounts) => {
     })
 })
 
-contract("Greeter: update greeting", () => {
+contract("Greeter: update greeting", (accounts) => {
     describe("setGreeting(string)", () => {
-        it("sets greeting to passed in string", async () => {
+        describe("when message is send by the owner", () => {
+            it("sets greeting to passed in string", async () => {
+                const greeter = await GreeterContract.deployed();
+                const expected = "The owner changed the message";
+
+                await greeter.setGreeting(expected);
+                const actual = await greeter.greet();
+
+                assert.equal(actual, expected, "greeting updated");
+            })
+        })
+    })
+
+    describe("when message is send by another account", () => {
+        it("does not set the greeting", async () => {
             const greeter = await GreeterContract.deployed();
-            const expected = "Hi, there!";
+            const expected = await greeter.greet();
 
-            await greeter.setGreeting(expected);
-            const actual = await greeter.greet();
-
-            assert.equal(actual, expected, "greeting was not updated");
+            try {
+                await greeter.setGreeting("Not the owner", { from: accounts[1] });
+            } catch(err) {
+                const errorMessage = "Ownable: caller is not the owner";
+                assert.equal(err.reason, errorMessage, "greeting should not update");
+                return;
+            }
+            assert(false, "greeting should not update");
         })
     })
 })
