b40b5b23a58b3687c28120f43073569a194417ec
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-08 08:39:05 +0900

    Add fallback function

--- fundriser/contracts/Fundraiser.sol
+++ fundriser/contracts/Fundraiser.sol
@@ -82,4 +82,9 @@ contract Fundraiser is Ownable {
         beneficiary.transfer(balance);
         emit Withdraw(balance);
     }
+
+    fallback () external payable {
+        totalDonations = totalDonations.add(msg.value);
+        donationsCount = donationsCount.add(1);
+    }
 }
