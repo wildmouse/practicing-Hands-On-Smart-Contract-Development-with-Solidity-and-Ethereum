14b23f8116d2852460008b33e9eae07cce955aeb
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-11 15:15:24 +0900

    Add getting fundraisers

--- fundriser/contracts/FundraiserFactory.sol
+++ fundriser/contracts/FundraiserFactory.sol
@@ -5,7 +5,21 @@ import "./Fundraiser.sol";
 contract FundraiserFactory {
     Fundraiser[]  private _fundraisers;
 
+    uint256 constant maxLimit = 20;
+
     event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
+
+    function fundraisers(uint256 limit, uint256 offset)
+    public
+    view
+    returns (Fundraiser[] memory fundraisers)
+    {
+        uint256 size = fundraisersCount() < limit ? fundraisersCount() : limit;
+        size = size < maxLimit ? size : maxLimit;
+        fundraisers = new Fundraiser[](size);
+
+        return fundraisers;
+    }
 
     function fundraisersCount() public view returns (uint256) {
         return _fundraisers.length;
--- fundriser/test/fundraiser_factory_test.js
+++ fundriser/test/fundraiser_factory_test.js
@@ -54,3 +54,73 @@ contract("FundraiserFactory: createFundraiser", (accounts) => {
     )
   })
 })
+
+contract("FundraiserFactory: fundraisers", (accounts) => {
+   async function createFundraiserFactory(fundraiserCount, accounts) {
+     const factory = await FundraiserFactoryContract.new();
+     await addFundraisers(factory, fundraiserCount, accounts);
+     return factory
+   }
+
+   async function addFundraisers(factory, count, accounts) {
+     const name = "Beneficiary";
+     const lowerCaseName = name.toLowerCase();
+     const beneficiary = accounts[1];
+
+     for (let i = 0; i < count; i++) {
+       await factory.createFundraiser(
+         `${name} ${i}`,
+         `${lowerCaseName}${i}.example.com`,
+         `${lowerCaseName}${i}.png`,
+         `Description for ${name} ${i}`,
+         beneficiary
+       )
+     }
+   }
+
+   describe("when fundraisers collection is empty", () => {
+     it ("returns an empty collection", async () => {
+       const factory = await createFundraiserFactory(0, accounts);
+       const fundraisers = await factory.fundraisers(10, 0);
+       assert.equal(
+         fundraisers.length,
+         0,
+         "collection should be empty"
+       )
+     })
+   })
+
+  describe("varying limits", async () => {
+    let factory;
+    beforeEach(async () => {
+      factory = await createFundraiserFactory(30, accounts);
+    })
+
+    it("returns 10 results when limit requested is 10", async () => {
+      const fundraisers = await factory.fundraisers(10, 0);
+      assert.equal(
+        fundraisers.length,
+        10,
+        "results size should be 10"
+      )
+    })
+
+    it("returns 20 results when limit requested is 20", async () => {
+      const fundraisers = await factory.fundraisers(20, 0);
+      assert.equal(
+        fundraisers.length,
+        20,
+        "result size should be 20"
+      )
+    })
+
+    it("returns 20 results when limit requested is 30", async () => {
+      const fundraisers = await factory.fundraisers(30, 0);
+      assert.equal(
+        fundraisers.length,
+        20,
+        "result size should be 20"
+      )
+    })
+  })
+})
