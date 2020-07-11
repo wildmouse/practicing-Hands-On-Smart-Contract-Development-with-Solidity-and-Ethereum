5a7ad0a67227f00b612b6edd4d737738a5614949
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-12 08:07:22 +0900

    Add greeter contract

---
+++ greeter-dapp/contracts/Greeter.sol
@@ -0,0 +1,15 @@
+pragma solidity >= 0.6.0 < 0.7.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
+    string private _greeting = "Hello, World!";
+
+    function greet() external view returns(string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner {
+        _greeting = greeting;
+    }
+}
