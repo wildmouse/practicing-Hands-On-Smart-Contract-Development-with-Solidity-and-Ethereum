453d048f5c67119ef011d677155c1066ebc338aa
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-11 10:52:55 +0900

    Add creating fundraiser

--- fundriser/contracts/FundraiserFactory.sol
+++ fundriser/contracts/FundraiserFactory.sol
@@ -1,5 +1,29 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-contract FundraiserFactory {
+import "./Fundraiser.sol";
 
+contract FundraiserFactory {
+    Fundraiser[]  private _fundraisers;
+
+    function fundraisersCount() public view returns (uint256) {
+        return _fundraisers.length;
+    }
+
+    function createFundraiser(
+        string memory name,
+        string memory url,
+        string memory imageURL,
+        string memory description,
+        address payable beneficiary
+    ) public {
+        Fundraiser fundraiser = new Fundraiser(
+            name,
+            url,
+            imageURL,
+            description,
+            beneficiary,
+            msg.sender
+        );
+        _fundraisers.push(fundraiser);
+    }
 }
--- fundriser/test/fundraiser_factory_test.js
+++ fundriser/test/fundraiser_factory_test.js
@@ -6,3 +6,32 @@ contract("FundraiserFactory: deployment", () => {
     assert(fundraiserFactory, "fundraiser factory was not deployed");
   })
 })
+
+contract("FundraiserFactory: createFundraiser", (accounts) => {
+  let fundraiserFactory;
+  // fundraiser args
+  const name = "Beneficiary Name";
+  const url = "beneficiaryname.example.com";
+  const imageURL = "https://placekitten.com/600/350";
+  const description = "Beneficiary Description";
+  const beneficiary = accounts[1];
+
+  it("increments the fundraisersCount", async() => {
+    fundraiserFactory = await FundraiserFactoryContract.deployed();
+    const currentFundraisersCount = await fundraiserFactory.fundraisersCount();
+    await fundraiserFactory.createFundraiser(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary
+    )
+    const newFundraisersCount = await fundraiserFactory.fundraisersCount();
+
+    assert.equal(
+      newFundraisersCount - currentFundraisersCount,
+      1,
+      "should increment by 1"
+    )
+  })
+})
